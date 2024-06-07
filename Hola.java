6fc2878aa05bc820620e683f1576e63ebb1582cb
Alex <alejandromirandaco1801@gmail.com>
2024-06-07 10:37:37 +0200

    Prueba

--- Hola.java
+++ Hola.java
@@ -2,5 +2,8 @@ public class Hola {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
